d86d5f7a4808d8b258501082209e66c7f225f541
Bastian Gruber <gruberbastian@me.com>
2019-02-20 19:56:57 +0100

    Impl Trait

--- bob/src/lib.rs
+++ bob/src/lib.rs
@@ -1,42 +1,33 @@
+trait Style {
+    fn asking(&self) -> bool;
+    fn yelling(&self) -> bool;
+    fn nothing(&self) -> bool;
+}
+
+
+impl Style for str {
+    fn asking(&self) -> bool {
+        self.trim().ends_with('?')
+    }
+    fn yelling(&self) -> bool {
+        self.to_uppercase() == self && self.to_lowercase() != self
+    }
+    fn nothing(&self) -> bool {
+        self.trim().is_empty()
+    }
+}
+
+
 pub fn reply(message: &str) -> &str {
-    let m = String::from(message);
-    
-    if m.is_empty() || m.trim().is_empty() {
-        return "Fine. Be that way!"
-    }
-
-    let contains_lowercase = m.find(|c: char| c.is_lowercase());
-    let contains_alphabetic = m.find(|c: char| c.is_alphabetic());
-    
-    let all_uppercase = contains_lowercase == None;
-    let last: String = m.chars().last().unwrap().to_string();
-    let is_uppercase: bool = m.chars().last().unwrap().is_uppercase();
-    
-    if &*last == " " && m.trim().chars().last().unwrap().to_string() == "?" {
-        return "Sure."
-    }
-
-    if contains_alphabetic == None && &*last == "?" {
-        return "Sure."
-    }
-    
-    if contains_alphabetic == None {
-        return "Whatever."
-    }
-
-    if all_uppercase && &*last == "?" {
-        return "Calm down, I know what I'm doing!"
-    }
-    
-    if all_uppercase {
-        return "Whoa, chill out!"
-    }
-    
-    match &*last {
-        "." => "Whatever.",
-        "!" => "Whatever.",
-        " " => "Whatever.",
-        "?" => "Sure.",
-        _ => "Fine. Be that way!"
+    if message.nothing() {
+        "Fine. Be that way!"
+    } else if message.yelling() && message.asking() {
+        "Calm down, I know what I'm doing!"
+    } else if message.asking() {
+        "Sure."
+    } else if message.yelling() {
+        "Whoa, chill out!"
+    } else {
+        "Whatever."
     }
 }
