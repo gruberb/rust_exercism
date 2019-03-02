5ada874db66a1211ea6bab3d5c3e74cd23fef044
Bastian Gruber <gruberbastian@me.com>
2019-03-02 08:30:27 +0100

    Finishing "proverb"

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,6 +1,16 @@
 pub fn build_proverb(list: &[&str]) -> String {
+    let mut result: Vec<String> = Vec::new();
     
-    for word in list.iter() {
+    for (index, word) in list.iter().enumerate() {
+        let mut sentence = String::from("");
+        if list.len() - 1 == index {
+            sentence.push_str(&format!("And all for the want of a {}.", list[0]));
+        } else {
+            sentence.push_str(&format!("For want of a {} the {} was lost.", list[index], list[index+1]));
+        }
 
+        result.push(sentence);
     }
+    
+    result.join("\n")
 }
